3edd8ac9ece79a26142666f84fc35372c4ce0466
p1ckless <putrasahaka343@gmail.com>
2025-09-15 22:49:49 +0700

    Semester 1

---
+++ aritmatika.py
@@ -0,0 +1,12 @@
+panjang_kebun_jeruk = 75 
+lebar_kebun_jeruk = 15
+
+luas_petak = 25
+karung_petak = 2
+
+luas_kebun_jeruk = panjang_kebun_jeruk * lebar_kebun_jeruk
+
+karung_dibutuhkan = luas_kebun_jeruk // luas_petak * karung_petak
+
+print(f"Karung Pupuk yang diperlukan Pak Tono adalah {karung_dibutuhkan}")
+
---
+++ tipedata.py
@@ -0,0 +1,12 @@
+# |======================================|
+
+aplikasi = "WhatsApp"    
+print(f"ini {aplikasi}")
+
+total_download = 5000000000
+print(f"total download {aplikasi} adalah {total_download}") 
+
+rating = 4,3
+print(f"rating dari aplikasi {aplikasi} adalah {rating}") 
+
+# |======================================|
