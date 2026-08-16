8773553c0fe0f22bae0a1c9ce061940f01f1c872
p1ckless <putrasahaka343@gmail.com>
2026-08-16 19:53:17 +0700

    add: tugas baru C#

---
+++ C#/kasirRestoran.cs
@@ -0,0 +1,62 @@
+int totalBayar = 0;
+string pilihan = " ";
+
+while (pilihan != "selesai")
+{
+    Console.WriteLine("=== Menu Yang Tersedia ===");
+    Console.WriteLine("1. Nasi Goreng, Rp15.000");
+    Console.WriteLine("2. Ayam Goreng, Rp12.000");
+    Console.WriteLine("3. Es Teh, Rp4.000");
+    Console.WriteLine("4. Es Jeruk Hangat, Rp3.000");
+    Console.WriteLine("5. Es Milo, Rp6.000");
+
+    Console.Write("Masukkan pilihanmu / ketik 'selesai' untuk keluar dari program : ");
+    pilihan = Console.ReadLine().ToLower().Trim();
+    if (pilihan == "selesai")
+    {
+        break;
+    }
+    int nomor_menu = int.Parse(pilihan);
+    string nama_menu = " ";
+    int harga_menu = 0;
+
+    if (nomor_menu == 1)
+    {
+        nama_menu = "Nasi Goreng";
+        harga_menu = 15000;
+    }
+    else if (nomor_menu == 2)
+    {
+        nama_menu = "Ayam Goreng";
+        harga_menu = 12000;
+    }
+    else if (nomor_menu == 3)
+    {
+        nama_menu = "Es Teh";
+        harga_menu = 4000;
+    }
+    else if (nomor_menu == 4)
+    {
+        nama_menu = "Es Jeruk Hangat";
+        harga_menu = 3000;
+    }
+    else if (nomor_menu == 5)
+    {
+        nama_menu = "Es Milo";
+        harga_menu = 6000;
+    }
+    else
+    {
+        Console.WriteLine("Masukkan nomor yang valid");
+        continue;
+    }
+    Console.Write("Masukkan jumlah pesanan kamu :");
+    int jumlah_pesanan = int.Parse(Console.ReadLine());
+    int total_bayar = jumlah_pesanan * harga_menu;
+
+    Console.WriteLine("==================");
+    Console.WriteLine("Pesanan Kamu" + " " + nama_menu);
+    Console.WriteLine("Jumlah Pesanan Kamu" + " " + jumlah_pesanan);
+    Console.WriteLine("Total Bayar" + " " + total_bayar);
+    Console.WriteLine("==================");
+}
